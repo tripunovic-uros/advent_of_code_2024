db7a86ce3fcf2522daec3d96287d0b49c0460dd7
Uros <uros.tripunovic@omegapoint.se>
2024-12-04 23:16:25 +0100

    Complete Day 2

--- challenges/day2/part1/aoc.go
+++ challenges/day2/part1/aoc.go
@@ -2,16 +2,60 @@ package part1
 
 import (
 	_ "embed"
+	"math"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
 //go:embed input
 var input string
 
+// //go:embed sample_input
+// var input string
+
 func Solve() string {
-	return "TODO"
+	reports := parse()
+	var safe int
+	for _, report := range reports {
+		if isSafe(report) {
+			safe++
+		}
+	}
+	return strconv.Itoa(safe)
 }
 
-func parse() {
+func isSafe(numbers []string) bool {
+	a, _ := strconv.Atoi(numbers[0])
+	b, _ := strconv.Atoi(numbers[len(numbers)-1])
+	increasing := isIncreasing(a, b)
+	isSafe := true
+	for i := 1; i < len(numbers); i++ {
+		a, _ := strconv.Atoi(numbers[i-1])
+		b, _ := strconv.Atoi(numbers[i])
+		sum := int(math.Abs(float64(a) - float64(b)))
+		if increasing != isIncreasing(a, b) || sum == 0 || sum > 3 {
+			isSafe = false
+			break
+		}
+	}
+	return isSafe
+}
+
+func isIncreasing(a, b int) bool {
+	return a < b
+}
+
+func parse() [][]string {
 	input = strings.TrimSpace(input)
+	rows := strings.Split(input, "\n")
+	regex := regexp.MustCompile(`\s+`)
+
+	arr := make([][]string, len(rows))
+
+	for i, row := range rows {
+		row := regex.Split(row, -1)
+		arr[i] = row
+	}
+	return arr
 }
--- challenges/day2/part2/aoc.go
+++ challenges/day2/part2/aoc.go
@@ -2,16 +2,68 @@ package part2
 
 import (
 	_ "embed"
+	"math"
+	"regexp"
+	"slices"
+	"strconv"
 	"strings"
 )
 
 //go:embed input
 var input string
 
+// //go:embed sample_input
+// var input string
+
 func Solve() string {
-	return "TODO"
+	reports := parse()
+	var safe int
+	for _, report := range reports {
+		for i := range report {
+			// sub_slice := append(append([]string{}, report[:i]...), report[i+1:]...)
+			copy_report_slice := slices.Clone(report)
+			sub_report_slice := slices.Delete(copy_report_slice, i, i+1)
+			if isSafe(sub_report_slice) {
+				safe++
+				break
+			}
+		}
+
+	}
+	return strconv.Itoa(safe)
 }
 
-func parse() {
+func isSafe(numbers []string) bool {
+	a, _ := strconv.Atoi(numbers[0])
+	b, _ := strconv.Atoi(numbers[len(numbers)-1])
+	increasing := isIncreasing(a, b)
+	isSafe := true
+	for i := 1; i < len(numbers); i++ {
+		a, _ := strconv.Atoi(numbers[i-1])
+		b, _ := strconv.Atoi(numbers[i])
+		sum := int(math.Abs(float64(a) - float64(b)))
+		if increasing != isIncreasing(a, b) || sum == 0 || sum > 3 {
+			isSafe = false
+			break
+		}
+	}
+	return isSafe
+}
+
+func isIncreasing(a, b int) bool {
+	return a < b
+}
+
+func parse() [][]string {
 	input = strings.TrimSpace(input)
+	rows := strings.Split(input, "\n")
+	regex := regexp.MustCompile(`\s+`)
+
+	arr := make([][]string, len(rows))
+
+	for i, row := range rows {
+		row := regex.Split(row, -1)
+		arr[i] = row
+	}
+	return arr
 }
