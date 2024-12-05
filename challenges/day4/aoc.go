743e53de124d38e682cebd01403bf6fb208f0026
Uros <uros.tripunovic@omegapoint.se>
2024-12-05 20:19:30 +0100

    Prepare Day 4

---
+++ challenges/day4/aoc.go
@@ -0,0 +1,31 @@
+package day4
+
+import (
+	_ "embed"
+	"regexp"
+	"strings"
+)
+
+//go:embed input
+var input string
+
+// //go:embed sample_input
+// var input string
+
+func SolvePartOne() string {
+	return "TODO"
+}
+
+func SolvePartTwo() string {
+	return "TODO"
+}
+
+func parse() {
+	input = strings.TrimSpace(input)
+	rows := strings.Split(input, "\n")
+	regex := regexp.MustCompile(`\s+`)
+
+	for i, row := range rows {
+		row := regex.Split(row, -1)
+	}
+}
