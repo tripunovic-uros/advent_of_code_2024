8073df6b931d650bf8d135cf942191aac2941aa6
Uros <uros.tripunovic@omegapoint.se>
2024-12-05 20:15:31 +0100

    Complete Day 3

--- challenges/day3/aoc.go
+++ challenges/day3/aoc.go
@@ -3,6 +3,7 @@ package day3
 import (
 	_ "embed"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -13,19 +14,66 @@ var input string
 // var input string
 
 func SolvePartOne() string {
-	return "TODO"
+	muls, _ := parse()
+	sum := compute(muls)
+	return strconv.Itoa(sum)
 }
 
 func SolvePartTwo() string {
-	return "TODO"
+	_, muls := parse()
+	sum := compute(muls)
+	return strconv.Itoa(sum)
 }
 
-func parse() {
+func compute(muls [][]string) int {
+	regex := regexp.MustCompile(`\d{1,3}`)
+	var sum int
+	for _, row := range muls {
+		for _, mul := range row {
+			parameters := regex.FindAllString(mul, -1)
+			lhs := parameters[0]
+			rhs := parameters[1]
+			l, _ := strconv.Atoi(lhs)
+			r, _ := strconv.Atoi(rhs)
+			sum += l * r
+		}
+	}
+	return sum
+}
+
+func parse() ([][]string, [][]string) {
 	input = strings.TrimSpace(input)
 	rows := strings.Split(input, "\n")
-	regex := regexp.MustCompile(`\s+`)
 
-	for i, row := range rows {
-		row := regex.Split(row, -1)
+	var parsed_part_one [][]string
+	regex := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+
+	var parsed_part_two [][]string
+	regex_two := regexp.MustCompile(`(do\(\)|don't\(\)|(mul\(\d{1,3},\d{1,3}\)))`)
+	should_append := true
+
+	for _, row := range rows {
+		part_one := regex.FindAllString(row, -1)
+		parsed_part_one = append(parsed_part_one, part_one)
+
+		part_two := regex_two.FindAllString(row, -1)
+		var new_part_two []string
+		for _, instruction := range part_two {
+			if instruction == "do()" {
+				should_append = true
+				continue
+			}
+			if instruction == "don't()" {
+				should_append = false
+				continue
+			}
+			if !should_append {
+				continue
+			}
+			new_part_two = append(new_part_two, instruction)
+		}
+		parsed_part_two = append(parsed_part_two, new_part_two)
 	}
+
+	return parsed_part_one, parsed_part_two
 }
