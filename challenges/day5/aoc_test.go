cf2fd32b29595d02bba94300d9472ac74e4a9620
Uros <uros.tripunovic@omegapoint.se>
2024-12-08 17:49:04 +0100

    Prepare Day 5

---
+++ challenges/day5/aoc_test.go
@@ -0,0 +1,22 @@
+package day5
+
+import (
+	_ "embed"
+	"testing"
+)
+
+func TestSolvePartOne(t *testing.T) {
+	expected_part_one := "Replace here"
+	result := SolvePartOne()
+	if result != expected_part_one {
+		t.Errorf("Expected %s, got %s", expected_part_one, result)
+	}
+}
+
+func TestSolvePartTwo(t *testing.T) {
+	expected_part_two := "Replace here"
+	result := SolvePartTwo()
+	if result != expected_part_two {
+		t.Errorf("Expected %s, got %s", expected_part_two, result)
+	}
+}
